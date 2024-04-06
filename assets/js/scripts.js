1132134107bed87f6ec04fcf11f01bcab2a992a1
Nick Bruggeman <nick.bruggeman92@gmail.com>
2024-04-06 14:10:54 -0700

    started modals

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,45 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+const navbar = document.querySelector(".navbar");
+const dropdownButton = document.querySelector(".dropbtn");
+// const entryModal = document.querySelector(".addEntryModal");
+const dropdownContent = document.querySelector("#myDropdown");
+const addEntryModal = document.querySelector("#addEntryModal");
+const findModal = document.querySelector("#findModal");
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
+function displayNavBar() {
+  if (dropdownContent.classList.contains("show")) {
+    dropdownContent.classList.replace("show", "dropdown-content")
+  } else {
+    dropdownContent.classList.replace("dropdown-content", "show")
   }
+}
+function displayModal(event) {
+  const selection = event.target.dataset.modal
+    if (selection === "addEntry") {
+      addEntryModal.classList.add("display")
+      dropdownContent.classList.replace("show", "dropdown-content")
+    } else if (selection === "find") {
+      findModal.classList.add("display")
+      dropdownContent.classList.replace("show", "dropdown-content")
+    } else {
+      dropdownContent.classList.replace("show", "dropdown-content")
+    }
+}
+dropdownButton.addEventListener("click", displayNavBar);
+
+dropdownContent.addEventListener("click", displayModal);
