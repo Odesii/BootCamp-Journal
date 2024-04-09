f6326c9c95f599b11e6fe6210e9ed257dd4b010d
Gabriela Ortiz <gabrielaortiz@Gabrielas-Air.lan>
2024-04-09 14:57:49 -0700

    connected the giphy t the modal so it can search up gifs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,6 +19,10 @@ function newModal() {
                       <div class="control">
                           <input class="input" type="text" id="gif"/>
                       </div>
+                      <!-- giphy button -->
+                    <button class="move" id="search-giphy">Search</button>
+                    <!-- here is where the giphy is displayed with the 'li' and 'img' elements -->
+                    <div id="giphyDiv"></div>
                   </div>
                   <div class="field">
                       <label class="label" for="date">Date</label>
@@ -49,6 +53,37 @@ function newModal() {
     modal.querySelector('.modal-close').addEventListener('click', function() {
       modal.classList.remove('is-active');
     });
+
+// G's work --> #sorry alic
+// Giphy werk (;
+const giphyDiv = document.getElementById('giphyDiv')
+const giphyAPIKey = 'x6k9Oyo3vy4ha1dy1xYlxVuo79ltCB6t'
+
+// GIPHY SEARCH FUNCTION
+function getGiphy(topic) {
+    const giphyURL = `https://api.giphy.com/v1/stickers/search?api_key=x6k9Oyo3vy4ha1dy1xYlxVuo79ltCB6t&q=${topic}&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+// fetching giphy data and looping please note 'data' element is a element for giphy as well hence the data.data line
+fetch(giphyURL).then(response => response.json())
+    .then(data => {
+        console.log(data); // this line helps figure out if its working in the console
+        giphyDiv.innerHTML = ''; // this line
+        data.data.forEach(giphy => {
+        const giphyImg = document.createElement('img'); // this line creates a new image element in the HTML
+        giphyImg.src = giphy.images.original.url; // this line will use the giphy url to display the giphs
+        giphyDiv.appendChild(giphyImg);  
+
+    });
+
+})}
+
+    modal.querySelector("#search-giphy").addEventListener("click", function(event){
+    // Function to get giphy to work in modal    
+    event.preventDefault()   
+   const topic = modal.querySelector("#gif").value
+   console.log(topic)
+    getGiphy(topic)
+
+    })
   }
   
   document.getElementById('addBtn').addEventListener('click', newModal);
