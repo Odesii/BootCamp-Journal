f66e7ff3e69389a951e8b7aa970bb300204f362c
Odesii <alic.madd@gmail.com>
2024-04-10 19:02:41 -0400

    Started styling on posts page.

--- assets/js/blogentries.js
+++ assets/js/blogentries.js
@@ -1,4 +1,4 @@
-const mainElement = document.querySelector(".container");
+const mainElement = document.querySelector(".postContainer");
 
 // retrieving data from submitted entries
 const readBlogDataFromLocalStorage = function () {
@@ -14,30 +14,39 @@ const renderBlogList = function () {
   const blogEntries = readBlogDataFromLocalStorage();
 
   const article = document.createElement("article");
+  article.classList.add("posts");
 
   for (let index = 0; index < blogEntries.length; index++) {
-    const date = document.createElement("div");
+    const cardContainer = document.createElement("div");
     const title = document.createElement("h3");
+    const date = document.createElement("div");
     const content = document.createElement("p");
     const gif = document.createElement("img");
+    const article = document.createElement("article");
+    cardContainer.classList.add("posts");
+    article.classList.add("mainCard");
 
-    date.textContent = blogEntries[index].date;
-    article.appendChild(date);
-
+    title.classList.add("titleCard")
     title.textContent = blogEntries[index].title;
-    article.appendChild(title);
+    // article.appendChild(title);
+
+    date.setAttribute("id", "date");
+    date.textContent = blogEntries[index].date;
+    // article.appendChild(date);
 
     content.textContent = blogEntries[index].content;
-    article.appendChild(content);
+    // article.appendChild(content);
 
     gif.setAttribute("src", blogEntries[index].gif);
-    article.appendChild(gif);
-    
-    // article.classList.add("card");
+    // article.appendChild(gif);
 
+    cardContainer.append(date, content, gif);
+
+    // article.classList.add("card");
+    article.append(title, cardContainer);
     mainElement.appendChild(article);
   }
-  
 };
 
 renderBlogList();
+
